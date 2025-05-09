3c78eb629bf73331f57d8918c06a67bb99f95c55
GNIvanov21 <105590079+GNIvanov21@users.noreply.github.com>
2025-05-09 10:13:36 +0300

    Add db models

---
+++ API/JobTracking.DataAccess/Models/worker.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using JobTracking.DataAccess.Data.Base;
+using JobTracking.DataAccess.Models;
+
+namespace JobTracking.DataAccess.Data.Models;
+public class worker : IEntity
+{    
+    [Key]   
+    public int Id { get; set; }
+    public bool IsActive { get; set; }    
+    public DateTime CreatedOn { get; set; }
+    [Required]
+    public string CreatedBy { get; set; }
+    public DateTime? UpdatedOn { get; set; }    
+    public string? UpdatedBy { get; set; }    
+    [Required]    
+    public int CompanyId { get; set; }    
+    public virtual company Company { get; set; }
+    
+}
